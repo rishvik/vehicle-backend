35bea9586ca5a342700862fffbacc08157ecb67f
rishvik <rishvik15@gmail.com>
2017-06-22 17:24:28 +0530

    myfirstapp

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var URL="mongodb://mukul:zxcvbnm@ds131782.mlab.com:31782/rish";
+const db=require('monk')(URL)
+const docs=db.get('pot')
+    /* GET home page. */
+router.get('/', function(req, res, next) {
+//  res.render('index', { title: 'Express' });
+    docs.insert ({"name":"rishvik"},function(err, docs){
+        if(err)console.log(err)
+        else  res.json(docs[0]);
+
+
+    })
+});
+
+module.exports = router;
